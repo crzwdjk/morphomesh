6d0a324bef1c296bc2bc4925c70442d83f920277
crzwdjk <crzwdjk@2da112dc-29ed-11de-a747-197756c87c9e>
2009-04-23 00:28:07 +0000

    First pass at skeleton structure

--- trunk/MeshSkeleton.h
+++ trunk/MeshSkeleton.h
@@ -12,10 +12,27 @@
 
 #include <milton.h>
 
+/* A mapping from a mesh vertex to its corresponding skeleton bone
+   specifies relative position in cylindrical coordinates 
+   (XXX: not sure this is a good idea) */
+struct SkeletonMapping {
+  int bone;          // the index of the skeleton bone
+  double t;          // distance along bone, range [0, 1]
+  double rotation;   // rotation around bone, range [0, 2pi)
+  double distance;   // distance from bone
+};
+
+
+stuct Bone {
+  int start_node;
+  int end_node;
+  Vector3 orient;    // basis for detrmining rotation about bone
+};
+
 class MeshSkeleton {
  public:
 
-  MeshSkeleton();
+  MeshSkeleton(Mesh & m) : m_mesh(m) {  }
 
   virtual ~MeshSkeleton();
 
@@ -32,9 +49,11 @@ class MeshSkeleton {
   }
 
  private:
-
   std::vector<Vector3> m_nodes;
+  std::vector<Bone> m_bones;
   
+  std::hash_map<int, SkeletonMapping> m_correspond;
+  Mesh & m_mesh;
 };
 
 #endif
