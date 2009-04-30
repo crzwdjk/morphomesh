eb8d6cecdd5402b9333cfd395408310f1dafc975
crzwdjk <crzwdjk@2da112dc-29ed-11de-a747-197756c87c9e>
2009-04-30 02:54:50 +0000

    update mesh on drag now that update is fast

--- trunk/Frontend.cpp
+++ trunk/Frontend.cpp
@@ -101,7 +101,6 @@ void Frontend::mousePressEvent  (InteractionInfo &info) {
        if (min_distance < drag_thresh / getWidth()) {
 	   m_deforming = true;
 	   m_deformingVert = closest_point;
-	   m_oldPos = skel->GetNodes()[closest_point];
 	   cerr << "drag " << closest_point << endl;
        }
    }
@@ -143,8 +142,8 @@ void Frontend::mouseMoveEvent  (InteractionInfo &info) {
        Ray r = getCamera()->getWorldRay(Point2(pt[0] / getWidth(), pt[1] / getHeight(), 1));
        Vector3 d = Point3() + skel->GetNodes()[m_deformingVert] - r.origin;
        Vector3 newpos = (r.origin + d.getMagnitude() * r.direction) - Point3();
-       skel->GetNodes()[m_deformingVert] = newpos;
 
+       skel->update(m_deformingVert, newpos);
        m_parent->redraw();
        return;
    }
@@ -174,9 +173,6 @@ void Frontend::mouseReleaseEvent(InteractionInfo &info) {
 
    if (m_deforming) {
        m_deforming = false;
-       Vector3 newpos = skel->GetNodes()[m_deformingVert];
-       skel->GetNodes()[m_deformingVert] = m_oldPos;
-       skel->update(m_deformingVert, newpos);
        m_parent->redraw();
    }
 #if 0
